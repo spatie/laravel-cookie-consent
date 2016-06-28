0baa93c98fa9af76a5bfbd827069331d96ef27bf
freek <freek@spatie.be>
2016-06-28 10:50:00 +0200

    wip

--- tests/CookieConsentTest.php
+++ tests/CookieConsentTest.php
@@ -5,11 +5,24 @@ namespace Spatie\CookieConsent\Test;
 class CookieConsentTest extends TestCase
 {
     /** @test */
-    public function it_()
+    public function it_provides_translations()
     {
-        $view = view('layout')->render();
-
-        dd($view);
+        $this->assertTranslationExists('cookieConsent::texts.message');
+        $this->assertTranslationExists('cookieConsent::texts.button_text');
     }
 
+    /** @test */
+    public function it_can_display_a_cookie_consent_view()
+    {
+        $html = view('layout')->render();
+
+        $this->assertConsentDialogDisplayed($html);
+    }
+
+    public function it_will_not_show_the_cookie_consent_view_when_the_package_is_disabled()
+    {
+        $this->app['config']->set('laravel-cookie-consent.enabled', false);
+
+        $this;
+    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,4 +24,43 @@ abstract class TestCase extends OrchestraTestCase
         $app['config']->set('laravel-blade-javascript.namespace', 'js');
         $app['config']->set('view.paths', [__DIR__.'/stubs/views']);
     }
+
+    /**
+     * @param $key
+     *
+     * @return bool
+     */
+    public function assertTranslationExists($key)
+    {
+        $this->assertTrue(trans($key) != $key, "Failed to assert that a translation exists for key `{$key}`");
+    }
+
+    /**
+ * @param string $html
+ */
+    protected function assertConsentDialogDisplayed($html)
+    {
+        $this->assertTrue($this->isConsentDialogDisplayed($html), 'Failed to assert that the consent dialog is displayed.');
+    }
+
+    /**
+     * @param string $html
+     */
+    protected function assertConsentDialogIsNotDisplayed($html)
+    {
+        $this->assertFalse($this->isConsentDialogDisplayed($html), 'Failed to assert that the consent dialog is not being displayed.');
+    }
+
+    /**
+     * @param string $html
+     *
+     * @return bool
+     */
+    protected function isConsentDialogDisplayed($html)
+    {
+        return str_contains($html, [
+            trans('cookieConsent::texts.message'),
+            trans('cookieConsent::texts.button_text'),
+        ]);
+    }
 }
