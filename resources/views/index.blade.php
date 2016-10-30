944697ba89d651bc5fceb1a3bf166b3428e7f30e
benmag <benmag@users.noreply.github.com>
2016-10-31 03:55:31 +1000

    Fix for issue #15
    * Fix BFCache issue
    
    Prevents the accepted dialog from showing again when using the back
    button
    
    * Rename checkCookie() to cookieExists() and move it to after the body of consentWithCookies()
    
    * Fix: correctly check if cookie exists
    
    * fix (again) correctly check
    
    * Split cookie string with `; `
    
    Javascript uses `; ` not just `;`.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,15 @@
 
         window.laravelCookieConsent = (function () {
 
+            var COOKIE_VALUE = 1;
+
             function consentWithCookies() {
-                setCookie('{{ $cookieConsentConfig['cookie_name'] }}', 1, 365 * 20);
+                setCookie('{{ $cookieConsentConfig['cookie_name'] }}', COOKIE_VALUE, 365 * 20);
                 hideCookieDialog();
+            }
+
+            function cookieExists(name) {
+                return (document.cookie.split('; ').indexOf(name + '=' + COOKIE_VALUE) !== -1);
             }
 
             function hideCookieDialog() {
@@ -23,6 +29,10 @@
                 var date = new Date();
                 date.setTime(date.getTime() + (expirationInDays * 24 * 60 * 60 * 1000));
                 document.cookie = name + '=' + value + '; ' + 'expires=' + date.toUTCString() +';path=/';
+            }
+
+            if(cookieExists('{{ $cookieConsentConfig['cookie_name'] }}')) {
+                hideCookieDialog();
             }
 
             var buttons = document.getElementsByClassName('js-cookie-consent-agree');
