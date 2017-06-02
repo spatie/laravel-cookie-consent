38903551e2ef3e5aea834e14e649888be7d7b8bb
Salvatore Pruiti <salvatorepruiti@hotmail.com>
2017-06-02 16:59:25 +0200

    Added Italian Language

---
+++ resources/lang/it/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'Questo sito utilizza i cookies per offrirti un\'esperienza di navigazione migliore. Usando il nostro servizio accetti l\'impiego di cookie in accordo con la nostra cookie policy.',
+    'agree' => 'Consenti',
+];
