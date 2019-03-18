535835534436ef9583aca10cfd3d998b9464b388
dmcbrn <40859618+dmcbrn@users.noreply.github.com>
2019-03-18 23:24:09 +0100

    Minor spaces changes

--- config/cookie-consent.php
+++ config/cookie-consent.php
@@ -17,12 +17,11 @@ return [
      * Set the cookie duration in days.  Default is 365 * 20.
      */
     'cookie_lifetime' => 365 * 20,
-    
-    
+
     /*
      * Excluded routes
      * */
     'excluded' => [
         //'excluded-route-name',
-    ]
+    ],
 ];
