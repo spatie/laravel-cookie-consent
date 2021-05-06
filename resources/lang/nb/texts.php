79000db945693a30c0ec3afeff674958acecd187
Daniel S. Billing <daniel@infihex.com>
2021-05-06 10:08:14 +0200

    Update Norwegian translations

--- resources/lang/nb/texts.php
+++ resources/lang/nb/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => 'Vi bruker informasjonskapsler (cookies) som lagres i nettleseren din når du besøker nettsiden vår. Ved å fortsette å besøke nettsiden vår eller bruke tjenestene våre, godtar du at vi bruker informasjonskapsler.',
-    'agree' => 'Jeg forstår',
+    'message' => 'Din opplevelse på dette nettstedet vil bli forbedret ved å tillate informasjonskapsler (cookies).',
+    'agree' => 'Tillat informasjonskapsler',
 ];
