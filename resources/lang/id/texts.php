d8d1090432a2be2edf89a2fc3021d99b236eca30
Zainal Hasan <19884603+zhanang19@users.noreply.github.com>
2021-08-19 08:47:32 +0700

    fix: corrected typo on `id` translation

--- resources/lang/id/texts.php
+++ resources/lang/id/texts.php
@@ -2,5 +2,5 @@
 
 return [
     'message' => 'Pengalaman anda pada situs ini akan meningkat dengan cara mengizinkan cookies.',
-    'agree' => 'Izikan Cookies',
+    'agree' => 'Izinkan Cookies',
 ];
