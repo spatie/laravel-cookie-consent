fab412595e75d1eea3231a4ac0ffadfc71141b2e
Alex Manase <malexandru699@gmail.com>
2023-01-06 18:21:46 +0200

    refactor: CookieConsentMiddlewareTest

--- tests/CookieConsentMiddlewareTest.php
+++ tests/CookieConsentMiddlewareTest.php
@@ -1,130 +1,111 @@
 <?php
 
-namespace Spatie\CookieConsent\Test;
-
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Spatie\CookieConsent\CookieConsentMiddleware;
 
-class CookieConsentMiddlewareTest extends TestCase
-{
-    /** @test */
-    public function it_injects_the_if_a_closing_body_tag_is_found()
-    {
-        $request = new Request();
+it('injects the if a closing body tag is found', function () {
+    $request = new Request();
 
-        $middleware = new CookieConsentMiddleware();
+    $middleware = new CookieConsentMiddleware();
 
-        $result = $middleware->handle($request, function ($request) {
-            return (new Response())->setContent('<html><head></head><body></body></html>');
-        });
+    $result = $middleware->handle($request, function ($request) {
+        return (new Response())->setContent('<html><head></head><body></body></html>');
+    });
 
-        $content = $result->getContent();
+    $content = $result->getContent();
 
-        $this->assertStringContainsString('<html><head></head><body>', $content);
-        $this->assertStringContainsString('window.laravelCookieConsent', $content);
-        $this->assertStringContainsString('</body></html>', $content);
-    }
+    expect($content)->toContain(
+        '<html><head></head><body>',
+        'window.laravelCookieConsent',
+        '</body></html>'
+    );
+});
 
-    /** @test */
-    public function it_does_not_alter_content_that_does_not_contain_a_body_tag()
-    {
-        $request = new Request();
+it('does not alter content that does not contain a body tag', function () {
+    $request = new Request();
 
-        $middleware = new CookieConsentMiddleware();
+    $middleware = new CookieConsentMiddleware();
 
-        $result = $middleware->handle($request, function ($request) {
-            return (new Response())->setContent('<html></html>');
-        });
+    $result = $middleware->handle($request, function ($request) {
+        return (new Response())->setContent('<html></html>');
+    });
 
-        $content = $result->getContent();
+    expect($result->getContent())->toContain('<html></html>');
+});
 
-        $this->assertEquals('<html></html>', $content);
-    }
+it('does not use a secure cookie if session secure is false', function () {
+    config()->set('session.secure', false);
 
-    /** @test */
-    public function it_does_not_use_a_secure_cookie_if_session_secure_is_false()
-    {
-        config(['session.secure' => false]);
+    $middleware = new CookieConsentMiddleware();
 
-        $middleware = new CookieConsentMiddleware();
+    $result = $middleware->handle(new Request(), function () {
+        return (new Response())->setContent('<html><head></head><body></body></html>');
+    });
 
-        $result = $middleware->handle(new Request(), function () {
-            return (new Response())->setContent('<html><head></head><body></body></html>');
-        });
+    expect($result->getContent())
+        ->toContain(';path=/\'')
+        ->not->toContain(';path=/;secure\'');
+});
 
-        $this->assertStringContainsString(';path=/\'', $result->getContent());
-        $this->assertStringNotContainsString(';path=/;secure\'', $result->getContent());
-    }
+it('uses a secure cookie if config session is set to secure', function () {
+    config(['session.secure' => true]);
 
-    /** @test */
-    public function it_uses_a_secure_cookie_if_config_session_is_set_to_secure()
-    {
-        config(['session.secure' => true]);
+    $middleware = new CookieConsentMiddleware();
 
-        $middleware = new CookieConsentMiddleware();
+    $result = $middleware->handle(new Request(), function () {
+        return (new Response())->setContent('<html><head></head><body></body></html>');
+    });
 
-        $result = $middleware->handle(new Request(), function () {
-            return (new Response())->setContent('<html><head></head><body></body></html>');
-        });
+    expect($result->getContent())
+        ->not->toContain(';path=/\'')
+        ->toContain(';path=/;secure\'');
+});
 
-        $this->assertStringNotContainsString(';path=/\'', $result->getContent());
-        $this->assertStringContainsString(';path=/;secure\'', $result->getContent());
-    }
+test('the cookie domain is set by the session domain config variable', function () {
+    config(['session.domain' => 'some domain']);
 
-    /** @test */
-    public function the_cookie_domain_is_set_by_the_session_domain_config_variable()
-    {
-        config(['session.domain' => 'some domain']);
+    $middleware = new CookieConsentMiddleware();
 
-        $middleware = new CookieConsentMiddleware();
+    $result = $middleware->handle(new Request(), function () {
+        return (new Response())->setContent('<html><head></head><body></body></html>');
+    });
 
-        $result = $middleware->handle(new Request(), function () {
-            return (new Response())->setContent('<html><head></head><body></body></html>');
-        });
+    expect($result->getContent())->toContain('const COOKIE_DOMAIN = \'some domain\'');
+});
 
-        $this->assertStringContainsString('const COOKIE_DOMAIN = \'some domain\'', $result->getContent());
-    }
+test('the cookie "samesite" attribute is not set if config session is set to false', function () {
+    config(['session.same_site' => null]);
 
-    /** @test */
-    public function the_cookie_samesite_attribute_is_not_set_if_config_session_is_set_to_false()
-    {
-        config(['session.same_site' => null]);
+    $middleware = new CookieConsentMiddleware();
 
-        $middleware = new CookieConsentMiddleware();
+    $result = $middleware->handle(new Request(), function () {
+        return (new Response())->setContent('<html><head></head><body></body></html>');
+    });
 
-        $result = $middleware->handle(new Request(), function () {
-            return (new Response())->setContent('<html><head></head><body></body></html>');
-        });
+    expect($result->getContent())->not->toContain(';samesite=');
+});
 
-        $this->assertStringNotContainsString(';samesite=', $result->getContent());
-    }
+test('the cookie "samesite" attribute is by the session "samesite" config variable', function () {
+    config(['session.same_site' => 'strict']);
 
-    /** @test */
-    public function the_cookie_samesite_attribute_is_by_the_session_samesite_config_variable()
-    {
-        config(['session.same_site' => 'strict']);
+    $middleware = new CookieConsentMiddleware();
 
-        $middleware = new CookieConsentMiddleware();
+    $result = $middleware->handle(new Request(), function () {
+        return (new Response())->setContent('<html><head></head><body></body></html>');
+    });
 
-        $result = $middleware->handle(new Request(), function () {
-            return (new Response())->setContent('<html><head></head><body></body></html>');
-        });
+    expect($result->getContent())->toContain(';samesite=strict');
+});
 
-        $this->assertStringContainsString(';samesite=strict', $result->getContent());
-    }
+it('uses the request host unless session domain is set', function () {
+    config(['session.domain' => null]);
 
-    /** @test */
-    public function it_uses_the_request_host_unless_session_domain_is_set()
-    {
-        config(['session.domain' => null]);
+    $middleware = new CookieConsentMiddleware();
 
-        $middleware = new CookieConsentMiddleware();
+    $result = $middleware->handle(new Request(), function () {
+        return (new Response())->setContent('<html><head></head><body></body></html>');
+    });
 
-        $result = $middleware->handle(new Request(), function () {
-            return (new Response())->setContent('<html><head></head><body></body></html>');
-        });
-
-        $this->assertStringContainsString('const COOKIE_DOMAIN = \'localhost\'', $result->getContent());
-    }
-}
+    expect($result->getContent())->toContain('const COOKIE_DOMAIN = \'localhost\'');
+});
