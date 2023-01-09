c5222319d5467d636ea1d0472cb29adcc498291e
Alexandru Manase <malexandru699@gmail.com>
2023-01-09 14:51:40 +0200

    Convert all tests to pest (#186)
    * install Pest
    
    * refactor: CookieConsentTest
    
    * refactor: CookieConsentMiddlewareTest
    
    * feat: replace test suite in Github action and `composer.json`
    
    * fix: use Str helper class instead of str()
    
    * Fix styling
    
    Co-authored-by: alexmanase <alexmanase@users.noreply.github.com>

---
+++ tests/Pest.php
@@ -0,0 +1,41 @@
+<?php
+
+use \Illuminate\Support\Str;
+use function PHPUnit\Framework\assertFalse;
+use function PHPUnit\Framework\assertTrue;
+
+uses(Spatie\CookieConsent\Test\TestCase::class)->in('.');
+
+// Functions
+
+function assertTranslationExists(string $key)
+{
+    assertTrue(
+        trans($key) != $key,
+        "Failed to assert that a translation exists for key `{$key}`"
+    );
+}
+
+function assertConsentDialogDisplayed(string $html)
+{
+    assertTrue(
+        isConsentDialogDisplayed($html),
+        'Failed to assert that the consent dialog is displayed.'
+    );
+}
+
+function assertConsentDialogIsNotDisplayed(string $html)
+{
+    assertFalse(
+        isConsentDialogDisplayed($html),
+        'Failed to assert that the consent dialog is not being displayed.'
+    );
+}
+
+function isConsentDialogDisplayed(string $html): bool
+{
+    return Str::contains($html, [
+        trans('cookie-consent::texts.message'),
+        trans('cookie-consent::texts.button_text'),
+    ]);
+}
